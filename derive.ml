8acb6eb6b037290b5f80fbbba4bc8bfc9fa3e4f7
ip <igor@ahrefs.com>
2018-03-19 16:14:02 +0200

    more aggregation types

--- derive.ml
+++ derive.ml
@@ -7,6 +7,7 @@ module U = Json.Util
 type result_type = [
   | `List of result_type
   | `Dict of (string * result_type) list
+  | `Assoc of (result_type * result_type)
   | `Int
   | `String
   | `Double
@@ -17,6 +18,9 @@ type agg_type =
 | Cardinality of string
 | Terms of string
 | Histogram of string
+| Filter
+| Filters
+| Top_hits
 
 type single_aggregation = { name : string; agg : agg_type; }
 type aggregation = { this : single_aggregation; sub : aggregation list; }
@@ -29,8 +33,11 @@ let analyze_single_aggregation name agg_type json =
     match agg_type with
     | "max" | "min" | "avg" | "sum" -> Simple_metric (field ())
     | "cardinality" -> Cardinality (field ())
-    | "terms" -> Terms (field ())
+    | "terms" | "significant_terms" -> Terms (field ())
     | "histogram" -> Histogram (field ())
+    | "filter" -> Filter
+    | "filters" -> Filters
+    | "top_hits" -> Top_hits
     | _ -> Exn.fail "unknown aggregation type %S" agg_type
   in
   { name; agg; }
@@ -59,7 +66,6 @@ let rec aggregation (name,x) =
     exn -> Exn.fail ~exn "while analyzing aggregation %S" name
 
 let get_aggregations x =
-  let open U in
   extract_aggregations x |> fst |> List.map aggregation
 
 let infer_single_aggregation { name; agg; } sub =
@@ -70,6 +76,9 @@ let infer_single_aggregation { name; agg; } sub =
     | Cardinality _field -> [], sub ["value", `Int ]
     | Terms field -> [], buckets (`Typeof field)
     | Histogram field -> [`Is_num field], buckets `Double
+    | Filter -> [], sub ["doc_count", `Int]
+    | Filters -> [], `Dict [ "buckets", `Assoc (`String, sub ["doc_count", `Int])]
+    | Top_hits -> [], `Dict [ "hits", `Dict [ "total", `Int ] ]
   in
   cstr, (name, shape)
 
@@ -86,7 +95,7 @@ let atd_of_es_type = function
 | "keyword" -> `String
 | s -> Exn.fail "atd_of_es_type: cannot handle %S" s
 
-let rec resolve_types properties shape : result_type =
+let resolve_types properties shape : result_type =
   let typeof t =
     match List.assoc t properties with
     | exception _ -> Exn.fail "no field %S in schema" t
@@ -95,6 +104,7 @@ let rec resolve_types properties shape : result_type =
   let rec map = function
   | `List t -> `List (map t)
   | `Dict fields -> `Dict (List.map (fun (n,t) -> n, map t) fields)
+  | `Assoc (k,v) -> `Assoc (map k, map v)
   | `Typeof x -> typeof x
   | `Int | `String | `Double as t -> t
   in
@@ -109,6 +119,7 @@ module Gen = struct
   open Atd_ast
   let loc = dummy_loc
   let list t = `List (loc,t,[])
+  let tuple l = `Tuple (loc, List.map (fun t -> (loc,t,[])) l, [])
   let record fields = `Record (loc,fields,[])
   let field n t = `Field (loc, (n, `Required, []), t)
   let pname t params = `Name (loc,(loc,t,params),[])
@@ -139,6 +150,7 @@ let atd_of_shape name (shape:result_type) =
     | `Dict ["key",k; "doc_count", `Int] -> pname "doc_count" [map k]
     | `Dict ["buckets", `List t] -> pname "buckets" [map t]
     | `Dict fields -> push @@ record (List.map (fun (n,t) -> field n (map t)) fields)
+    | `Assoc (k,v) -> list (tuple [map k; map v])
   in
   tuck types (typ name (map ~push:id shape));
   (loc,[]), List.rev !types
