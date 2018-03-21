de4ec90826e680b42b599a54bb642e1697d11910
ip <igor@ahrefs.com>
2018-03-21 12:29:41 +0200

    generate wraps

--- esgg.ml
+++ esgg.ml
@@ -3,4 +3,5 @@ module Json = Yojson.Safe
 let () =
   match Action.args with
   | ["derive";mapping;query] -> Derive.derive (Json.from_file ~fname:mapping mapping) (Json.from_file ~fname:query query)
+  | ["derive";name;mapping;query] -> Derive.derive ~name (Json.from_file ~fname:mapping mapping) (Json.from_file ~fname:query query)
   | _ -> assert false
