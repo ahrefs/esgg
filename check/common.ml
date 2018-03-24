ab61b538da7b846040248cee5822c05b3c731069
ip <igor@ahrefs.com>
2018-03-24 18:18:00 +0200

    naive hygiene

--- check/common.ml
+++ check/common.ml
@@ -1,4 +1,4 @@
-module Id(T : sig type t end) : sig
+module Id_(T : sig type t end) : sig
   type t
   val wrap : T.t -> t
   val unwrap : t -> T.t
@@ -10,10 +10,11 @@ struct
 end
 
 (* ES types *)
-module Long = struct type t = int end
-module Double = struct type t = float end
-module Keyword = String
-module Text = String
-module Date = String
-module Boolean = struct type t = bool end
-module Ip = String
+module Long_ = struct type t = int end
+module Int64_ = struct type t = int64 end
+module Double_ = struct type t = float end
+module Keyword_ = String
+module Text_ = String
+module Date_ = String
+module Boolean_ = struct type t = bool end
+module Ip_ = String
