2d838dbf8c477e96443832d7aad1e332e006b751
ip <igor@ahrefs.com>
2018-06-07 14:21:01 -0700

    add unit tests

---
+++ test/run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+#set -x
+#set -e
+
+# cd self directory
+cd "${0%/*}"
+
+cmd () { if ! "$@" ; then printf "FAILED: %s\n" "$*" >&2; exit 2; fi }
+
+function run() {
+  dir=$(dirname $1)
+  echo -n "$dir "
+  (
+  set -e
+  cmd ../esgg.native output $dir/mapping.json $dir/query.json > $dir/output.atd
+  cmd ../esgg.native vars $dir/mapping.json $dir/query.json > $dir/input.atd
+  ) && echo "... ok"
+}
+
+if [ $# -eq 0 ]; then
+  for p in */mapping.json ; do
+    run "$p"
+  done
+else
+  for i in "$@"; do
+    run "$i"
+  done
+fi
+
+cmd git diff --no-ext-diff --quiet --exit-code */*
