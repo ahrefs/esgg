c16af88deef81623c980dc3e4ef43a3c83b74334
ip <igor@ahrefs.com>
2018-07-18 01:06:46 -0700

    build with atd 1.13.0

--- derive.mli
+++ derive.mli
@@ -4,6 +4,6 @@ open Common
 
 val derive : mapping -> Tjson.t -> input_vars * (string -> string) * Tjson.t
 
-val output : mapping -> Tjson.t -> Atd_ast.full_module
+val output : mapping -> Tjson.t -> Atd.Ast.full_module
 
 val print_reflect : string -> Yojson.Basic.json -> unit
