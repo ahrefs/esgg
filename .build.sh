f3bd41a7c03499e324e3c8c45aa398f922183392
ip <igor@ahrefs.com>
2019-02-12 10:42:30 +0200

    ci: fix separators

--- .build.sh
+++ .build.sh
@@ -3,11 +3,11 @@ set -e -u
 
 . /shared/ci-utils/opam-setup.sh
 
-echo "-- install opam deps"
+echo "--- install opam deps"
 cmd opam install . --deps-only
 
 echo "+++ build"
 cmd make clean build
 
-echo "++ test"
+echo "+++ test"
 cmd make test check
