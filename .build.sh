5efea69bd6ede78d82037825ef9fd97afe8b052b
ip <igor@ahrefs.com>
2019-02-12 10:47:02 +0200

    ci: remove self before building

--- .build.sh
+++ .build.sh
@@ -4,6 +4,8 @@ set -e -u
 . /shared/ci-utils/opam-setup.sh
 
 echo "--- install opam deps"
+# remove self to remove extra constraints
+cmd opam remove esgg
 cmd opam install . --deps-only
 
 echo "+++ build"
