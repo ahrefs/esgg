52ddb53998d1650eda26590dcf4a0fa44f58c61e
ip <igor@ahrefs.com>
2019-03-12 10:39:26 -0600

    check: fail if one of test cases fails

--- check/run.sh
+++ check/run.sh
@@ -10,33 +10,36 @@ cmd () { if ! "$@" ; then printf "FAILED: %s\n" "$*" >&2; exit 2; fi }
 
 function check {
   name=${1%.query.json}
-  dir=$(dirname $name)
-  echo $name
+  dir=$(dirname "$name")
+  echo "$name"
   (
   set -e
-  cmd ../_build/default/esgg.exe output -name $(basename $dir) $dir/mapping.json $name.query.json > check.atd
-  cmd cp check.atd $name.atd
+  cmd ../_build/default/esgg.exe output -name "$(basename "$dir")" "$dir/mapping.json" "$name.query.json" > check.atd
+  cmd cp check.atd "$name.atd"
   cmd atdgen -t -open Mapping check.atd
   cmd atdgen -j -open Mapping check.atd
-  cmd dune exec --root . ./run.bc -- < $name.result.json
+  cmd dune exec --root . ./run.bc -- < "$name.result.json"
   )
 }
 
 cmd dune build ../esgg.exe
 
+status=0
 if [ $# -eq 0 ]; then
   printf "include Common\n" > mapping.ml
   for p in **/mapping.json ; do
-    name=$(basename $(dirname $p))
+    name=$(basename "$(dirname "$p")")
     printf "\n" >> mapping.ml
-    cmd ../_build/default/esgg.exe reflect $name $p >> mapping.ml
+    cmd ../_build/default/esgg.exe reflect "$name" "$p" >> mapping.ml
   done
   for p in **/*.query.json ; do
-    check $p
+    check "$p" || status=3
   done
 else
-  for i in $@; do
-    check $i
+  for i in "$@"; do
+    check "$i" || status=3
   done
   # keep generated check.atd
 fi
+
+exit $status
